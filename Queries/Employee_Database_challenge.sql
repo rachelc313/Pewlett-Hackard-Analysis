048e9e48fe7a9710711c57ef8ce831131f039d44
rachelc313 <111570965+rachelc313@users.noreply.github.com>
2022-10-04 17:45:04 -0500

    created mentorship eligibility table

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,6 +1,3 @@
--- Drop table if exists
-DROP TABLE retirements_titles;
-
 -- Create new table with emp numbers, names, titles, start dates and end dates, filter by birthdates between 1952 and 1955
 SELECT e.emp_no,
 	e.first_name,
@@ -32,3 +29,18 @@ FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
 
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN title AS t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01' AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY (emp_no);
