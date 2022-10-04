254b6a3bc4ada6bacf4635fe569cd6a9455691b4
rachelc313 <111570965+rachelc313@users.noreply.github.com>
2022-10-04 17:26:14 -0500

    created retiring_title table

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -20,8 +20,15 @@ SELECT DISTINCT ON (emp_no) emp_no,
 	first_name,
 	last_name,
 	title
-
-INTO uniquw_titles
+INTO unique_titles
 FROM retirement_titles
 WHERE (to_date = '9999-01-01')
 ORDER BY emp_no, to_date DESC;
+
+-- Create table with count of each retiring title from unique_titles table
+SELECT COUNT (title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
